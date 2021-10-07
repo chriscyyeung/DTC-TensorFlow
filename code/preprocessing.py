0b7146d376d3fbe71bf4500fbfc3a2192776bda7
chriscyyeung <chriscyyeung@gmail.com>
2021-10-07 17:18:51 -0400

    Added preprocessing of left atrium images

---
+++ code/preprocessing.py
@@ -0,0 +1,51 @@
+import numpy as np
+import nrrd
+import h5py
+from glob import glob
+
+
+def process_dataset():
+    """Preprocessing of left atrial segmentation dataset images.
+
+    :return: None
+    """
+    output_size = [112, 112, 80]
+    image_list = glob("../data/2018_LA_Seg_Challenge/*/*/lgemri.nrrd")
+    for file in image_list:
+        image, img_header = nrrd.read(file)
+        print(image.shape)
+        seg, seg_header = nrrd.read(file.replace("lgemri.nrrd", "laendo.nrrd"))
+        print(seg.shape)
+        seg = (seg == 255).astype(np.uint8)
+        w, h, d = seg.shape
+
+        tempL = np.nonzero(seg)
+        minx, maxx = np.min(tempL[0]), np.max(tempL[0])
+        miny, maxy = np.min(tempL[1]), np.max(tempL[1])
+        minz, maxz = np.min(tempL[2]), np.max(tempL[2])
+
+        px = max(output_size[0] - (maxx - minx), 0) // 2
+        py = max(output_size[1] - (maxy - miny), 0) // 2
+        pz = max(output_size[2] - (maxz - minz), 0) // 2
+        minx = max(minx - np.random.randint(10, 20) - px, 0)
+        maxx = min(maxx + np.random.randint(10, 20) + px, w)
+        miny = max(miny - np.random.randint(10, 20) - py, 0)
+        maxy = min(maxy + np.random.randint(10, 20) + py, h)
+        minz = max(minz - np.random.randint(5, 10) - pz, 0)
+        maxz = min(maxz + np.random.randint(5, 10) + pz, d)
+
+        image = (image - np.mean(image)) / np.std(image)
+        image = image.astype(np.float32)
+        image = image[minx:maxx, miny:maxy]
+        seg = seg[minx:maxx, miny:maxy]
+        print(image.shape)
+        print(seg.shape)
+        f = h5py.File(file.replace('lgemri.nrrd', 'mri_norm2.h5'), 'w')
+        f.create_dataset('image', data=image, compression="gzip")
+        f.create_dataset('seg', data=seg, compression="gzip")
+        f.close()
+        break
+
+
+if __name__ == "__main__":
+    process_dataset()
