3f94d0d01185c7b2ed104851747a9cce4d965016
chriscyyeung <chriscyyeung@gmail.com>
2021-10-12 20:04:10 -0400

    Created model architecture; not complete

--- code/vnet.py
+++ code/vnet.py
@@ -4,34 +4,222 @@ import tensorflow as tf
 os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 
 
-class ConvBlock(tf.Module):
-    def __init__(self, n_stages, n_channels_out, input_shape, kernel_size=3, strides=1):
-        super(ConvBlock, self).__init__()
+class Block:
+    def __init__(self, inputs_shape, n_channels_out, kernel_size, strides, padding):
+        self.inputs_shape = inputs_shape
+        self.n_channels_out = n_channels_out
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = 1 if padding == "same" else 0
 
-        layers = []
+    def get_conv_shape(self):
+        new_shape = (
+            int((self.inputs_shape[0] - self.kernel_size + 2 * self.padding) / self.strides + 1),
+            int((self.inputs_shape[1] - self.kernel_size + 2 * self.padding) / self.strides + 1),
+            int((self.inputs_shape[2] - self.kernel_size + 2 * self.padding) / self.strides + 1),
+            self.n_channels_out
+        )
+        print(self.inputs_shape, new_shape)  # TESTING
+        return new_shape
+
+    # TODO: change calculation for upsampling
+    def get_deconv_shape(self):
+        pass
+
+
+class ConvBlock(tf.keras.layers.Layer, Block):
+    def __init__(self, n_stages, inputs_shape, n_channels_out, kernel_size=3, strides=1, padding="same"):
+        tf.keras.layers.Layer.__init__(self)
+        Block.__init__(self, inputs_shape, n_channels_out, kernel_size, strides, padding)
+
+        ops = []
         for i in range(n_stages):
             if i == 0:
-                layers.append(tf.keras.layers.Conv3D(n_channels_out, kernel_size, strides=strides,
-                                                     padding="same", input_shape=input_shape))
+                # Kaiwing He Uniform initialization is default in PyTorch
+                ops.append(tf.keras.layers.Conv3D(n_channels_out, kernel_size, strides=strides,
+                                                  padding=padding, input_shape=inputs_shape,
+                                                  kernel_initializer="he_uniform"))
             else:
-                layers.append(tf.keras.layers.Conv3D(n_channels_out, kernel_size, strides=strides, padding="same"))
-            layers.append(tf.keras.layers.BatchNormalization())
+                ops.append(tf.keras.layers.Conv3D(n_channels_out, kernel_size, strides=strides, padding="same",
+                                                     kernel_initializer="he_uniform"))
+            ops.append(tf.keras.layers.BatchNormalization())
             # residual function
             if i != n_stages - 1:
-                layers.append(tf.keras.layers.PReLU())
+                ops.append(tf.keras.layers.PReLU())
 
-        self.conv = tf.keras.Sequential(layers)
+        self.conv = tf.keras.Sequential(ops)
         self.relu = tf.keras.layers.PReLU()
 
-    def __call__(self, x):
+    def call(self, x):
+        # add input to output of last convolution
         x = (self.conv(x) + x)
         x = self.relu(x)
         return x
 
 
-class DownsamplingConvBlock(tf.Module):
-    def __init__(self):
-        super(DownsamplingConvBlock, self).__init__()
+class DownsamplingConvBlock(tf.keras.layers.Layer, Block):
+    def __init__(self, inputs_shape, n_channels_out, kernel_size=2, strides=2, padding="valid"):
+        tf.keras.layers.Layer.__init__(self)
+        Block.__init__(self, inputs_shape, n_channels_out, kernel_size, strides, padding)
 
-    def __call__(self, x):
-        pass
+        ops = [
+            tf.keras.layers.Conv3D(n_channels_out, kernel_size, strides=strides,
+                                   padding=padding, input_shape=inputs_shape,
+                                   kernel_initializer="he_uniform"),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.PReLU()
+        ]
+
+        self.conv = tf.keras.Sequential(ops)
+
+    def call(self, x):
+        x = self.conv(x)
+        return x
+
+
+class UpsamplingDeconvBlock(tf.keras.layers.Layer, Block):
+    def __init__(self, inputs_shape, n_channels_out, kernel_size=2, strides=2, padding="valid"):
+        tf.keras.layers.Layer.__init__(self)
+        Block.__init__(self, inputs_shape, n_channels_out, kernel_size, strides, padding)
+
+        ops = [
+            tf.keras.layers.Conv3DTranspose(n_channels_out, kernel_size, strides=strides,
+                                            padding=padding, input_shape=inputs_shape,
+                                            kernel_initializer="he_uniform"),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.PReLU()
+        ]
+
+        self.conv = tf.keras.Sequential(ops)
+
+    def call(self, x):
+        x = self.conv(x)
+        return x
+
+
+class OneByOneConvBlock(tf.keras.layers.Layer, Block):
+    def __init__(self, inputs_shape, n_channels_out, kernel_size=1, strides=1, padding="valid"):
+        tf.keras.layers.Layer.__init__(self)
+        Block.__init__(self, inputs_shape, n_channels_out, kernel_size, strides, padding)
+
+        self.conv = tf.keras.Sequential()
+        self.conv.add(
+            tf.keras.layers.Conv3D(n_channels_out, kernel_size, strides=strides,
+                                   padding=padding, input_shape=inputs_shape,
+                                   kernel_initializer="he_uniform")
+        )
+
+    def call(self, x):
+        x = self.conv(x)
+        return x
+
+
+class VNet(tf.keras.Model):
+
+    DROPOUT_RATE = 0.5
+
+    def __init__(self, inputs_shape, n_classes=1, n_filters=16, has_dropout=True):
+        super().__init__()
+        self.has_dropout = has_dropout
+
+        # left side
+        self.block_one = ConvBlock(1, inputs_shape, n_filters)
+        self.block_one_dw = DownsamplingConvBlock(self.block_one.get_conv_shape(), n_filters * 2)
+
+        self.block_two = ConvBlock(2, self.block_one_dw.get_conv_shape(), n_filters * 2)
+        self.block_two_dw = DownsamplingConvBlock(self.block_two.get_conv_shape(), n_filters * 4)
+
+        self.block_three = ConvBlock(3, self.block_two_dw.get_conv_shape(), n_filters * 4)
+        self.block_three_dw = DownsamplingConvBlock(self.block_three.get_conv_shape(), n_filters * 8)
+
+        self.block_four = ConvBlock(3, self.block_three_dw.get_conv_shape(), n_filters * 8)
+        self.block_four_dw = DownsamplingConvBlock(self.block_four.get_conv_shape(), n_filters * 16)
+
+        # bottom
+        self.block_five = ConvBlock(3, self.block_four_dw.get_conv_shape(), n_filters * 16)
+        self.block_five_up = UpsamplingDeconvBlock(self.block_five.get_deconv_shape(), n_filters * 8)
+
+        # right side
+        self.block_six = ConvBlock(3, self.block_five_up.get_conv_shape(), n_filters * 8)
+        self.block_six_up = UpsamplingDeconvBlock(self.block_six.get_deconv_shape(), n_filters * 4)
+
+        self.block_seven = ConvBlock(3, self.block_six_up.get_conv_shape(), n_filters * 4)
+        self.block_seven_up = UpsamplingDeconvBlock(self.block_seven.get_deconv_shape(), n_filters * 2)
+
+        self.block_eight = ConvBlock(2, self.block_seven_up.get_conv_shape(), n_filters * 2)
+        self.block_eight_up = UpsamplingDeconvBlock(self.block_eight.get_deconv_shape(), n_filters)
+
+        self.block_nine = ConvBlock(1, self.block_eight_up.get_conv_shape(), n_filters)
+
+        # 1x1x1 convolution
+        self.out_conv = OneByOneConvBlock(self.block_nine.get_output_shape(), n_classes)
+        self.out_conv2 = OneByOneConvBlock(self.block_nine.get_output_shape(), n_classes)
+
+        # regression head
+        self.tanh = tf.keras.layers.Activation("tanh", input_shape=self.out_conv.get_output_shape())
+
+        self.dropout = tf.keras.layers.Dropout(self.DROPOUT_RATE)
+
+    def encoder(self, input_volume):
+        x1 = self.block_one(input_volume)
+        x1_dw = self.block_one_dw(x1)
+
+        x2 = self.block_two(x1_dw)
+        x2_dw = self.block_two_dw(x2)
+
+        x3 = self.block_three(x2_dw)
+        x3_dw = self.block_three_dw(x3)
+
+        x4 = self.block_four(x3_dw)
+        x4_dw = self.block_four_dw(x4)
+
+        x5 = self.block_five(x4_dw)
+        if self.has_dropout:
+            x5 = self.dropout(x5)
+
+        return [x1, x2, x3, x4, x5]
+
+    def decoder(self, features):
+        x1 = features[0]
+        x2 = features[1]
+        x3 = features[2]
+        x4 = features[3]
+        x5 = features[4]
+
+        x5_up = self.block_five_up(x5)
+        x5_up = x5_up + x4
+
+        x6 = self.block_six(x5_up)
+        x6_up = self.block_six_up(x6)
+        x6_up = x6_up + x3
+
+        x7 = self.block_seven(x6_up)
+        x7_up = self.block_seven_up(x7)
+        x7_up = x7_up + x2
+
+        x8 = self.block_eight(x7_up)
+        x8_up = self.block_eight_up(x8)
+        x8_up = x8_up + x1
+
+        x9 = self.block_nine(x8_up)
+        if self.has_dropout:
+            x9 = self.dropout(x9)
+
+        # regression task
+        out = self.out_conv(x9)
+        out_tanh = self.tanh(out)
+
+        # classification task
+        out_seg = self.out_conv2(x9)
+
+        return out_tanh, out_seg
+
+    def call(self, input_volume):
+        features = self.encoder(input_volume)
+        out_tanh, out_seg = self.decoder(features)
+        return out_tanh, out_seg
+
+
+if __name__ == "__main__":
+    model = VNet((112, 112, 80, 1))
+    print(model.variables)
